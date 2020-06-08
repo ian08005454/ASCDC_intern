7de8c0682e2946c6fb1d4c2e9792d460bcaa41d9
Wei-Jie <ian08005454@gmail.com>
2020-06-08 23:46:55 +0800

    Updating site

--- excute.cmd
+++ excute.cmd
@@ -1,5 +1,4 @@
 cd /hugo/ASCDC_intern
-hugo -t airspace-hugo
 git add -A
 git commit -m "Updating site" && git push origin master
 git subtree push --prefix=public https://github.com/ian08005454/ASCDC_intern.git gh-pages
